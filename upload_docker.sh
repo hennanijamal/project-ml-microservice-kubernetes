004e4e7839c0584d73b5e1f3d1c2c1963e44a0f9
hennanijamal <51787027+hennanijamal@users.noreply.github.com>
2019-06-26 09:12:56 +0100

    updated upload_docker.sh file

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,15 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath=jamalhen/project-ml-microservice-kubernetes:latest
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login
+docker tag 4bb416642e01  $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+
+docker push jamalhen/project-ml-microservice-kubernetes:latest
