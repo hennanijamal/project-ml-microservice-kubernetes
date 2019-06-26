51fc5a366cf7efbf556823aa8ad49a087b8a2ada
hennanijamal <51787027+hennanijamal@users.noreply.github.com>
2019-06-26 17:26:39 +0100

    updated run_kuberbenets.sh file
    modified line 12 in the run_kubernetes.sh file

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -9,7 +9,7 @@ dockerpath=jamalhen/project-ml-microservice-kubernetes:latest
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-kubectl run project-ml-microservice-kubernetes --image=$dockerpath:latest --port=8000
+kubectl run --generator=run-pod/v1 --image=$dockerpath project-ml-microservice-kubernetes --port=8000
 
 # Step 3:
 # List kubernetes pods
