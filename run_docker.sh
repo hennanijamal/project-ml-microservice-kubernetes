ec370c66d1ba3d6cab0b80baf676823227d37558
hennanijamal <51787027+hennanijamal@users.noreply.github.com>
2019-06-26 09:00:28 +0100

    update run_docker.sh file
    updated file with the correct configurations

--- run_docker.sh
+++ run_docker.sh
@@ -4,12 +4,14 @@
 
 # Step 1:
 # Build image and add a descriptive tag
-docker build --tag=demolocal .
+sudo docker build --tag=microservice .
 
 # Step 2: 
 # List docker images
-docker image ls
+sudo docker image ls
+
+#docker run -it microservice bash
 
 # Step 3: 
 # Run flask app
-docker run -it demolocal bash
+sudo docker run -d -p 8000:80 microservice
