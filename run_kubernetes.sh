dcd34ea5b57eba930dfe76bce1d5ce8fc5336d30
hennanijamal <51787027+hennanijamal@users.noreply.github.com>
2019-06-26 09:00:42 +0100

    update run_kubernetes.sh file
    updated run_kubuernetes.sh file with correct configurations

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,16 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath=jamalhen/project-ml-microservice-kubernetes:latest
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run project-ml-microservice-kubernetes --image=$dockerpath:latest --port=8000
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward project-ml-microservice-kubernetes 8000:80
